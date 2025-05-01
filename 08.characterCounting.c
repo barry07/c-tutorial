eec6ac7f03daa2cbc7144d6f69249ef5597994c9
Barry Horgan <horgan.barry@gmail.comn>
2025-05-01 16:38:27 +1000

    In modern C (C99 and later), the main function must explicitly return int

--- 08.characterCounting.c
+++ 08.characterCounting.c
@@ -1,10 +1,17 @@
 // count number of characters in input
 
+// When running this in a terminal, to manually signal EOF, you typically press:
+// ctrl + Z
+
 #include <stdio.h>
 
-main ()
+int main (void)
 {
-    long nc // declare variable number of characters
-
+    long nc; // declare variable number of characters
     nc = 0; 
+    while (getchar() != EOF)
+        ++nc;
+    printf("%ld\n", nc);
+
+    return 0;
 }
