a24b8558f79e8de0384f8a83ecb3f1e169c66706
Barry Horgan <horgan.barry@gmail.comn>
2025-05-02 17:32:49 +1000

    else if!!

--- 10.exercise1.8.c
+++ 10.exercise1.8.c
@@ -4,5 +4,29 @@
 
 int main (void)
 {
+    int c; //character variable
+    int t, b, nl; //tab variable, blank variable, newline variable
+
+    t = 0;
+    b = 0;
+    nl = 0;
+
+    while ((c = getchar()) != EOF)
+        if (c == '\n' ) {
+            ++nl;
+        }
+            
+        else if (c == '\t') {
+            t++;
+        }
+            
+        else if (c == ' ') {
+            ++b;
+        }
+            
+    printf("%d\n", t);
+    printf("%d\n", b);  
+    printf("%d\n", nl); 
     
+    return 0;
 }
