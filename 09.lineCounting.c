5b5d12e9de94b28581e6576fd76b6edf00a59d26
Barry Horgan <horgan.barry@gmail.comn>
2025-05-02 16:40:51 +1000

    count newlines program=

---
+++ 09.lineCounting.c
@@ -0,0 +1,19 @@
+// count number of lines in input
+// 
+// When running this in a terminal, to manually signal EOF, you typically press:
+// ctrl + Z
+
+#include <stdio.h>
+
+int main(void)
+{
+    int c; // declare character variable
+    int nl; // declare newline counter variable
+
+    nl = 0; 
+
+    while ((c = getchar()) != EOF)
+        if (c == '\n')
+            ++nl;
+    printf('%d\n', nl);
+}
